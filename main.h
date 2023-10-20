95b75807a17cbfd662018d46edc97b27fc8f66c9
Mohammed Abdalla <dibshik55@gmail.com>
2023-10-20 06:45:27 -0700

    upload header file

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
+
+
+int _putchaar(char c);
+int _putts(char *c);
+int _printf(const char *format, ...);
+
+
+
+
+#endif
