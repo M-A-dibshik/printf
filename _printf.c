bef5ef243a6a7ebb9b5b3c526d6958a5ca9138a7
Mohammed Abdalla <dibshik55@gmail.com>
2023-10-20 07:37:52 -0700

    upload _printf.c

---
+++ _printf.c
@@ -0,0 +1,61 @@
+
+#include "main.h"
+
+/**
+ * _printf - prints anything
+ * @c: format string
+ * Return: number of bytes printed
+*/
+
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	va_list int_char;
+
+	if (format == NULL)
+		return -1;
+
+	va_start(int_char, format);
+
+	while (*format)
+	{
+		if (format != '%')
+		{
+		_putchaar(1, format, 1);
+			int_chat++;
+		}
+		else
+		{
+			format++;
+			if(*format == '\0')
+				break;
+
+			if (*format == '%')
+			{
+			_putchaar(1, format, 1);
+				int_char++;
+
+			}
+			else if(*format == 'c')
+			{
+				char c = va_arg(int_char, int);
+				_putchaar(1, &c, 1);
+				int_char++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(int_char, char*)
+				int str_len = 0;
+				
+				while (str[str_len] != '\0')
+					str_len++;
+				
+				_putts(1, str, str_len);
+				int_char += str_len;
+			}
+		}
+		format++;
+	}
+	va_end(format);
+	return (int_char);
+}
